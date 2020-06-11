f7e49f01c3c98af0779e6d5b27703bf14017f246
Richard Calf <richard.calf@gmail.com>
2020-06-11 17:44:24 +0200

    Add Module 3 - Comparing Employess pt 1

--- CollectIt/CollectIt/Program.cs
+++ CollectIt/CollectIt/Program.cs
@@ -10,13 +10,25 @@ namespace CollectIt
     {
         static void Main(string[] args)
         {
-            var employeesByDepartment = new SortedList<string, List<Employee>>();
-            employeesByDepartment.Add("Sales", new List<Employee> { new Employee(), new Employee(), new Employee() });
-            employeesByDepartment.Add("Engineering", new List<Employee> { new Employee(), new Employee() });
+            var departments = new SortedDictionary<string, List<Employee>>();
 
-            foreach(var item in employeesByDepartment)
+            departments.Add("Sales", new List<Employee> {
+                new Employee { Name = "Joy" },
+                new Employee { Name = "Dani" },
+                new Employee { Name = "Dani" }});
+
+            departments.Add("Engineering", new List<Employee> {
+                new Employee { Name = "Scott" },
+                new Employee { Name = "Alex" },
+                new Employee { Name = "Dani" }});
+            
+            foreach (var department in departments)
             {
-                Console.WriteLine($"The count of employees for {item.Key} is {item.Value.Count}");
+                Console.WriteLine(department.Key);
+                foreach (var employee in department.Value)
+                {
+                    Console.WriteLine($"\t{employee.Name}");
+                }
             }
 
             Console.ReadLine();
