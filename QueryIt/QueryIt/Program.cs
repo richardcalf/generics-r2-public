afd3a4aee426b9538855fa4fcb9339168e14652e
Richard Calf <richard.calf@gmail.com>
2020-06-12 15:28:42 +0200

    Add Module 5 - Commit and Query

--- QueryIt/QueryIt/Program.cs
+++ QueryIt/QueryIt/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,27 @@ namespace QueryIt
     {
         static void Main(string[] args)
         {
+            Database.SetInitializer(new DropCreateDatabaseAlways<EmployeeDb>());
+
+            using (IRepository<Employee> employeeRepository 
+                = new SqlRepository<Employee>(new EmployeeDb()))
+            {
+                AddEmployees(employeeRepository);
+                CountEmployees(employeeRepository);
+            }
+            Console.ReadLine();
+        }
+
+        private static void CountEmployees(IRepository<Employee> employeeRepository)
+        {
+            Console.WriteLine(employeeRepository.FindAll().Count());
+        }
+
+        private static void AddEmployees(IRepository<Employee> employeeRepository)
+        {
+            employeeRepository.Add(new Employee { Name = "Scott" });
+            employeeRepository.Add(new Employee { Name = "Chris" });
+            employeeRepository.Commit();
         }
     }
 }
