9b4769f21a602381f782d36f3ab4991fd68447f5
Richard Calf <richard.calf@gmail.com>
2020-06-12 15:19:17 +0200

    Add Module 5 - Setup EF and DataAccess layer

---
+++ QueryIt/QueryIt/DataAccess.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryIt
+{
+    public class EmployeeDb : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+    public interface IRepository<T> : IDisposable
+    {
+        void Add(T entity);
+        void Delete(T entity);
+        T FindById(int id);
+        IQueryable<T> FindAll();
+        int Commit();
+    }
+
+    public class SqlRepository<T> : IRepository<T> where T: class
+    {
+        DbContext _context;
+        DbSet<T> _set;
+        public SqlRepository(DbContext context)
+        {
+            _context = context;
+            _set = _context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _set.Add(entity);
+        }
+
+        public int Commit()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public IQueryable<T> FindAll()
+        {
+            return _set;
+        }
+
+        public T FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
