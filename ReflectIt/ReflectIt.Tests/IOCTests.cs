32d6118b0b47acc9d290477df122e041dbeb73f7
Richard Calf <richard.calf@gmail.com>
2020-06-12 19:58:50 +0200

    Add Module 6 - Simple Type Resolution - via IoCContainer

---
+++ ReflectIt/ReflectIt.Tests/IOCTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReflectIt.Tests
+{
+    [TestClass]
+    public class IOCTests
+    {
+        [TestMethod]
+        public void Can_Resolve_Types()
+        {
+            var ioc = new Container();
+            ioc.For<ILogger>().Use<SqlServerLogger>();
+
+            var logger = ioc.Resolve<ILogger>();
+
+            Assert.AreEqual(typeof(SqlServerLogger), logger.GetType());
+        }
+    }
+}
---
+++ ReflectIt/ReflectIt/Container.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectIt
+{
+    public class Container
+    {
+        Dictionary<Type, Type> _map = new Dictionary<Type, Type>();
+
+        public ContainerBuilder For<TSource>()
+        {
+            return For(typeof(TSource));
+        }
+
+        public ContainerBuilder For(Type sourceType)
+        {
+            return new ContainerBuilder(this, sourceType);
+        }
+
+        public TSource Resolve<TSource>()
+        {
+            return (TSource)Resolve(typeof(TSource));
+        }
+
+        public object Resolve(Type sourceType)
+        {
+            if (_map.ContainsKey(sourceType))
+            {
+                var destinationType = _map[sourceType];
+                return Activator.CreateInstance(destinationType);
+            }
+            else
+            {
+                throw new InvalidOperationException($"Could not resolve {sourceType.FullName}");
+            }
+        }
+
+        public class ContainerBuilder
+        {
+            Container _container;
+            Type _sourceType;
+
+            public ContainerBuilder(Container container, Type sourceType)
+            {
+                _container = container;
+                _sourceType = sourceType;
+            }
+
+            public ContainerBuilder Use<TDestination>()
+            {
+                return Use(typeof(TDestination));
+            }
+
+            public ContainerBuilder Use(Type destinationType)
+            {
+                _container._map.Add(_sourceType, destinationType);
+                return this;
+            }
+        }
+    }
+}
