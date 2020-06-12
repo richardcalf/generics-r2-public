70d0fb88de25f60cd83795c755248677d9b2aed9
Richard Calf <richard.calf@gmail.com>
2020-06-12 18:33:01 +0200

    Add Module 6 - ReflectIt Instanciating Generic Types v1

---
+++ ReflectIt/ReflectIt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeList = CreateCollection(typeof(List<Employee>));
+            Console.Write(employeeList.GetType().Name);
+            var genericArguments = employeeList.GetType().GenericTypeArguments;
+            foreach(var arg in genericArguments)
+            {
+                Console.Write($"[{arg.Name}]");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        private static object CreateCollection(Type type)
+        {
+            return Activator.CreateInstance(type);
+        }
+    }
+}
