dd74bbd63da4c089576c62acdde15e7a2c979384
Richard Calf <richard.calf@gmail.com>
2020-06-13 21:02:46 +0200

    Add Module 7 - The Math Problem

--- Tips/Tips/Program.cs
+++ Tips/Tips/Program.cs
@@ -6,31 +6,26 @@ using System.Threading.Tasks;
 
 namespace Tips
 {
-    public enum Steps
-    {
-        Step1,
-        Step2,
-        Step3        
-    }
-
-    public static class StringExtensions
-    {
-        public static TEnum ParseEnum<TEnum>(this string value) where TEnum: System.Enum
-        {
-            return (TEnum)Enum.Parse(typeof(TEnum), value);
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            var input = "Step1";
-            var value = input.ParseEnum<Steps>();
-
-            Console.WriteLine(value);
-            
+            var numbers = new double[] { 1, 2, 3, 4, 5, 6 };
+            var result = SampledAverage(numbers);
+            Console.WriteLine(result);
             Console.ReadLine();
+        }
+
+        private static double SampledAverage(double[] numbers)
+        {
+            var count = 0;
+            var sum = 0.0;
+            for (int i = 0; i < numbers.Length; i+=2)
+            {
+                sum += numbers[i];
+                count += 1;
+            }
+            return sum / count;
         }
     }
 }
