ba5817f8f4f31f77e8f4f48bc41767a58455c6bd
Richard Calf <richard.calf@gmail.com>
2020-06-13 20:35:10 +0200

    Add Module 7 - My non-generic enum parser

---
+++ Tips/Tips/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tips
+{
+    public enum Steps
+    {
+        Step1,
+        Step2,
+        Step3        
+    }
+
+    public static class StringExtensions
+    {
+        public static Steps ParseEnum(this string str)
+        {
+            return (Steps)Enum.Parse(typeof(Steps), str);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "Step1";
+            var value = (Steps)Enum.Parse(typeof(Steps), input);
+            var value2 = input.ParseEnum();
+
+            List<string> coll = new List<string>();
+            coll.Add(value.ToString());
+            coll.Add(value2.ToString());
+
+            foreach(var item in coll)
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
